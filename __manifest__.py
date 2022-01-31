b7c8cae9a5e835d987b663e1e212dcc7c8b870d8
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2022-01-31 14:48:48 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Last Sales Price',
+    'category': 'Accounting',
+    'version': '14.0.0.1',
+    'depends': [
+        'sale',
+        'purchase',
+        'account',
+    ],
+    'data': [
+        'views/view.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ models/model.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class SalesOrderLine(models.Model):
+
+    _inherit = 'sale.order.line'
+
+    last_sales_price = fields.Float(string='Last Price', compute='_compute_last_sales_price')
+
+    def _compute_last_sales_price(self):
+        move_line_obj = self.env['account.move.line'].sudo()
+        for line in self:
+            line.last_sales_price = 0
+            if line and line.product_id and line.order_id and line.order_id.partner_id:
+                invoice_line = move_line_obj.search([
+                    ('product_id', '=', line.product_id.id),
+                    ('move_id.move_type', '=', 'out_invoice'),
+                    ('move_id.partner_id', '=', line.order_id.partner_id.id),
+                ], order='id desc', limit=1)
+                if invoice_line:
+                    line.last_sales_price = invoice_line.price_unit
+
+    @api.onchange('order_id.partner_id', 'product_id')
+    def _onchange_data_change(self):
+        self._compute_last_sales_price()
+
+
+class AccountMoveLine(models.Model):
+
+    _inherit = 'account.move.line'
+
+    last_sales_price = fields.Float(string='Last Price', compute='_compute_last_sales_price')
+
+    def _compute_last_sales_price(self):
+        move_line_obj = self.env['account.move.line'].sudo()
+        for line in self:
+            line.last_sales_price = 0
+            if type(line.id) is int:
+                if line and line.product_id and line.move_id and line.move_id.partner_id:
+                    invoice_line = move_line_obj.search([
+                        ('id', '!=', line.id),
+                        ('product_id', '=', line.product_id.id),
+                        ('move_id.move_type', '=', 'out_invoice'),
+                        ('move_id.partner_id', '=', line.move_id.partner_id.id),
+                    ], order='id desc', limit=1)
+                    if invoice_line:
+                        line.last_sales_price = invoice_line.price_unit
+
+    @api.onchange('move_id.partner_id', 'product_id')
+    def _onchange_data_change(self):
+        self._compute_last_sales_price()
